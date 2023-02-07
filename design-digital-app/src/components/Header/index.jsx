ef87a01462091c3fd5fdf3789699fbcf38998ba0
ErickAraujodC <erickbyborges@gmail.com>
2023-02-06 23:43:17 -0300

    logo import

--- design-digital-app/src/components/Header/index.jsx
+++ design-digital-app/src/components/Header/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import './header.css'
+import './header.css';
+import logo from "../../src/assets/images/logo.png";
 
 const Header = (props) =>{
     return(
         <header className={props.isDarkTheme ? "header-dark" : "header-light"}>
-            <img className="logo" src="../../src/assets/images/logo.png" alt="Logo" />
+            <img className="logo" src={logo} alt="Logo" />
             
             <button onClick={props.changeTheme}>
                 <img className="theme-logo"  src={props.isDarkTheme ? "../../src/assets/images/sun.png" : "../../src/assets/images/moon.png" } alt="Imagem tema" />
