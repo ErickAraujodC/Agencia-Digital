0ddc365849c47b778ab8874243659180b9365869
ErickAraujodC <erickbyborges@gmail.com>
2023-02-06 23:46:03 -0300

    logo

--- design-digital-app/src/components/Header/index.jsx
+++ design-digital-app/src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './header.css';
-import logo from "../../src/assets/images/logo.png";
+import logo from "../../assets/images/logo.png";
 
 const Header = (props) =>{
     return(
